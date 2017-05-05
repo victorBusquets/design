0fc7fe08a6961d7a133eca9a51e12ad8f775a6d1
victorBusquets <victor.busquets.boro@gmail.com>
2017-05-05 15:39:00 +0200

    Solved bug for int split

--- public_html/admin-region/filters/cut.filter.js
+++ public_html/admin-region/filters/cut.filter.js
@@ -3,10 +3,12 @@
 
     angular.module('design.filters.cut', [])
 		.filter('cut', function(){
-			var defaultTail = ' ...';
+			var defaultTail = '...';
 			return function (value, max, tail) {
+				var numberType = (value ? value : '').constructor() === Number.prototype.constructor();
+				value = value ? value.toString() : '';
 				max = parseInt(max, 10);
-				return !value ? '' : !max || value.length <= max ? value : value.substr(0, max) + (tail || defaultTail);
+				return !max || value.length <= max ? value : value.substr(0, max) + ( numberType ? '' : ' ' ) +(tail || defaultTail);
 			};
 		});
 }());
