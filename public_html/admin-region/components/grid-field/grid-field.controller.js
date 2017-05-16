3410eafb24b4a5e86d2530a66999e2bcda101aaf
victorBusquets <victor.busquets.boro@gmail.com>
2017-05-16 12:40:52 +0200

    Add default option for new records

--- public_html/admin-region/components/grid-field/grid-field.controller.js
+++ public_html/admin-region/components/grid-field/grid-field.controller.js
@@ -3,11 +3,24 @@
 
     angular.module('design.components.grid-field.controller', [])
         .controller("GridFieldController", ["$scope", function( $scope ) {		
-			$scope.prepareSelectedValue = function(){
+			function filterSelectedValue(){
 				$scope.gridField.value = $scope.gridField.dependency.filter(function(element){
 					return element._id === ( $scope.gridField.value && $scope.gridField.value._id );
-				})[0] || $scope.gridField.dependency[0];
+				})[0] ;
+			};
+			
+			function addDefaultOption(){
+				$scope.gridField.dependency.unshift({
+					_id: null,
+					name: 'Seleccione una opción'
+				});
+				$scope.gridField.value = $scope.gridField.dependency[0];
+			};
+			
+			$scope.prepareSelectedValue = function(){
+				$scope.gridField.value ?  filterSelectedValue() : addDefaultOption();
 			};			
+			
 			
 			$scope.getPlaceholder = function(){
 				return $scope.gridField.disabled ? '' : $scope.gridField.title;
